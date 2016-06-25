ddaabd851f50f28c4874e2f9ebfd9561b6dbe9e7
Dan Turcza <datu925@gmail.com>
2016-06-25 21:25:13 +0700

    Finish MVP! Add popups, keyboard events, and click
    It's definitely buggy but the basic product works. A few must-have changes:
    1. There is no esc option to cancel once you've started. Furthermore, there's no handling for keys that don't have a corresponding link.
    2. For now I just did the letters a to z, so the mapping is not actually unique.
    3. Definitely some links (or link positions?) are surprising - I'll probably have to fiddle with how anchor tags are selected later.

---
+++ models/link.js
@@ -0,0 +1,8 @@
+function Link(link_details) {
+  // this.id = link_details["id"];
+  this.anchor = link_details["anchor"];
+  this.text = link_details["text"];
+  this.x = link_details["x"];
+  this.y = link_details["y"];
+  this.shortcut = null;
+}
--- shortcuts.js
+++ shortcuts.js
@@ -15,9 +15,85 @@ function isElementInViewport (el) {
     );
 }
 
+function genCharArray(charA, charZ) {
+    var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    for (; i <= j; ++i) {
+        a.push(String.fromCharCode(i));
+    }
+    return a;
+}
+
+
+
+function assignLinks(shortcutList, links) {
+    var i = 0;
+    var j = 0;
+    var shortcutHash = {};
+    while (i < links.length) {
+        var shortcut = shortcutList[j]
+        links[i].shortcut = shortcut;
+        shortcutHash[shortcut] = links[i];
+        i += 1;
+        if (j < 25) {
+            j += 1;
+        }
+    }
+    return shortcutHash;
+}
+
+function generatePopup(link) {
+    var div = $("<div class=popup>");
+    var txt = $("<span class=popup_text>");
+    return div.append(txt.text(link.shortcut))
+}
+
 
 $(function() {
   var anchors = $('a').filter(isElementInViewport);
-  anchors.css({ "background-color": "blue"});
+  // debugger;
+  var links = anchors.map(function() {
+    // debugger;
+    return new Link({"anchor": $(this), "text":$(this).innerText, "x": $(this).offset().left, "y": $(this).offset().top});
+    });
+
+  var shortcutList = genCharArray("a", "z");
+  var shortcutMap = assignLinks(shortcutList, links);
+
+  links.each(function() {
+    var popup = generatePopup(this);
+    $("body").append(popup);
+    popup.css({
+                "position":"absolute",
+                "top": this.y - 20, "left": this.x,
+                "background-color": "white",
+                "border": "1px solid black",
+                "padding": "10px",
+                "z-index": 10000
+                });
+  });
+  $("body").on("keyup", function(event) {
+    var keyCode = event.which;
+    var letter = String.fromCharCode(keyCode).toLowerCase();
+    var link = shortcutMap[letter];
+    // debugger;
+    link.anchor[0].click()
+    $(".popup").remove();
+    $("body").off("keyup");
+  })
+    // debugger;
+
+  /*
+
+  Procedure:
+
+    1. Find all links in current viewport. Done.
+    2. Freeze other possible user action. For instance, you can't click or fill out a form (or rather, doing so cancels script execution).
+    2. Take those links and assign letters than represent shortcuts to them. Done.
+    3. Overlay a small popup just over the original link containing their new shortcut. Done.
+    3. Upon key press, have jquery click the link and then finish script execution
+    4. Upon esc, exit the script.
+
+
+  */
 });
 
