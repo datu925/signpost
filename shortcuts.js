1ceb59e7a0a44bde23492135ee9e0e10c9d852ba
Dan Turcza <datu925@gmail.com>
2016-06-25 18:15:34 +0700

    Get script to run on icon click
    A JS script now runs when we click the icon, or when we click the shortcut I've assigned via Chrome. In the long run, we need to set that shortcut ourselves, but it's a start.
    
    The other long-term change is that right now we load jquery every time we want to run the script, and I suspect once the basic functionality is down I will try not to use jquery at all.

---
+++ shortcuts.js
@@ -0,0 +1,23 @@
+function isElementInViewport (el) {
+
+    //special bonus for those using jQuery
+    // if (typeof jQuery === "function" && el instanceof jQuery) {
+    //     el = el[0];
+    // }
+
+    var rect = this.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
+
+$(function() {
+  var anchors = $('a').filter(isElementInViewport);
+  anchors.css({ "background-color": "blue"});
+});
+
